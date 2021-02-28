1faee5f77fadae99c01411865ad8e0be4d895ec7
b_galloway <bgalloway@cadenceneuro.com>
2021-02-28 12:44:28 -0800

    lesson 8 assignment

--- blogging/models.py
+++ blogging/models.py
@@ -8,10 +8,13 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return "/latest/feed"
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from rest_framework import routers
+from api import views
+from blogging.feeds import LatestEntriesFeed
+from django.urls import reverse
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('polling/', include('polling.urls')),
@@ -8,4 +16,9 @@ urlpatterns = [
     path('', include('blogging.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('latest/feed/', LatestEntriesFeed())
 ]
+
+
